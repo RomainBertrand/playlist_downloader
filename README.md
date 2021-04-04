3b1f7a8b797da8fef6ee9e0eceb308f80651897a
RomainBertrand <>
2021-04-04 17:11:35 +0200

    renaming audio files

--- README.md
+++ README.md
@@ -2,16 +2,21 @@
 
 ## Dependencies
 
-- `ffmpeg` is required to merge the audio files. On ArchLinux, use
+- [`youtube_dl`](https://pypi.org/project/youtube_dl/) python package is required. In a virtual environment, `pip install youtube_dl`.
+
+- Optionnal: `ffmpeg` if you want to merge the audio files. On ArchLinux, use
 ```sudo-bash
 # pacman -S ffmpeg
 ```
 
-- [`youtube_dl`](https://pypi.org/project/youtube_dl/) python package is required. In a virtual environment, `pip install youtube_dl`.
-
-
 ## Usage
 
 ```bash
-./download_playlist.py https://youtube_playlist_link "name_to_save_the_playlist_to"
+./download_playlist.py https://youtube_playlist_link --playlist_name "name_to_save_the_playlist_to" --delete_files=False
 ```
+
+- `playlist_name`: By default, the playlist name will be the same as the one on Youtube.
+It will also be the name ofthe folder in which audio files will be kept if `delete_files=False`
+
+- `delete_files`: By default, will keep downloaded audio files.
+If set to `True`, will merge all songs into one using ffmpeg and delete the downloaded files.
--- download_playlist.py
+++ download_playlist.py
@@ -1,46 +1,63 @@
 #!/usr/bin/env python
 """Place this file in an empty folder"""
 import os
-import sys
+import argparse
 import youtube_dl
 
-def all_files(path: str) -> list:
-    """Given a path, return all files found in folder"""
-    # pre_processing
-    if path[-1] == "/":
-        path = path[:-1]
-    return [path+"/"+f for f in  os.listdir(path)]
 
-def videos_id(playlist_link: str, ydl: youtube_dl.YoutubeDL) -> list:
+def clean_title(video) -> str:
+    """Given a video object
+
+    Return a well formed string for the song title"""
+    title = video["title"]
+    if "(" in title and ")" in title:  # remove unnecessary elements, such as "(lyrics)"
+        opening_parenthesis_index = title.index("(")
+        closing_parenthesis_index = title.index(")")
+        title = title[:opening_parenthesis_index] + title[closing_parenthesis_index+1:]
+    if " - " not in title:  # add uploader if no author in initial title
+        title = video["uploader"] + " - " + title
+    return title
+
+
+def videos_id(playlist_link: str, ydl: youtube_dl.YoutubeDL, playlist_name=None, delete_files=True) -> (list, list, str):
     """Given a playlist link and a youtube_downloader,
 
-    Return the list of youtube ids in the playlist"""
+    Return the list of youtube ids in the playlist, a list of the titles, and the playlist name"""
     list_ids = []
-    result = ydl.extract_info \
-    (playlist_link,
-    download=False) #We just want to extract the info
+    list_titles = []
+    result = ydl.extract_info(playlist_link,
+                              download=False)  # We just want to extract the info
 
     if 'entries' in result:
+        if playlist_name is None:
+            playlist_name = result["playlist_name"]
         # Can be a playlist or a list of videos
         video = result['entries']
 
-        #loops entries to grab each video_url
+        # loops entries to grab each video_url
         for i, _ in enumerate(video):
             video = result['entries'][i]
             list_ids.append(video["id"])
-    return list_ids
+            if not delete_files:
+                list_titles.append(clean_title(video))
+    return list_ids, list_titles, playlist_name
+
 
 def merge_audio_files(ordered_list: list, output_file_name: str) -> None:
     """Given a list of ordered audio files, concatenate them using FFMPEG"""
-    ffmpeg_arguments = ' -i "concat:'
+    ffmpeg_arguments = ' -i "concat:'  # we concatenate all the provided files
     for audio in ordered_list:
-        assert os.path.exists(audio+".mp3"), "Error, an audio file doesn't exist"
+        assert os.path.exists(
+            audio+".mp3"), "Error, an audio file doesn't exist"
         ffmpeg_arguments += audio+'.mp3|'
-    ffmpeg_arguments = ffmpeg_arguments[:-1]
+    ffmpeg_arguments = ffmpeg_arguments[:-1]  # remove unecessary | symbol
     ffmpeg_arguments += '"'
-    os.system('ffmpeg {0} "{1}.mp3"'.format(ffmpeg_arguments, output_file_name))
+    os.system('ffmpeg {0} "{1}.mp3"'.format(
+        ffmpeg_arguments, output_file_name))
 
-def main():
+
+def download_playlist():
+    """Main function"""
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -53,20 +70,43 @@ def main():
     }
     youtube_downloader = youtube_dl.YoutubeDL(ydl_opts)
 
-    playlist_url = sys.argv[1]
-    playlist_name = sys.argv[2]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="playlist link")
+    parser.add_argument("--playlist_name", help="Force name for the final playlist \
+            or the directory in which files will be moved")
+    parser.add_argument(
+        "--delete_files", help="Delete downloaded audio files at the end of the program.\
+        If left at default, will rename the files")
+    args = parser.parse_args()
+    playlist_url = args.url
+    playlist_name = args.playlist_name
+    if args.delete_files is None or args.delete_files in ('false', 'False', '0'):
+        delete_files = False
+    elif args.delete_files in ('true', 'True', '1'):
+        delete_files = True
+    else:
+        raise TypeError("Couldn't convert delete_files value '{0}' to boolean".format(
+            args.delete_files))
 
     # First, we create a list of IDs of video, so that if there is an issue with FFMPEG
     # we don't have to download everything again.
-    video_ids_to_download = videos_id(playlist_url, youtube_downloader)
+    video_ids_to_download, video_titles, playlist_name = videos_id(
+        playlist_url, youtube_downloader, playlist_name, delete_files)
 
-    # download the files here
+    # download audio here
     youtube_downloader.download(video_ids_to_download)
 
-    # finally, merge all video files
+    # merge all audio files
     merge_audio_files(video_ids_to_download, playlist_name)
 
+    if delete_files:
+        for video_id in video_ids_to_download:
+            os.system("rm {0}.mp3".format(video_id))
+    else:  # rename
+        os.makedirs(playlist_name)
+        for i, video_id in enumerate(video_ids_to_download):
+            os.system('mv {0}.mp3 "{1}/{2}.mp3"'.format(video_id, playlist_name, video_titles[i]))
 
 
 if __name__ == '__main__':
-    main()
+    download_playlist()
